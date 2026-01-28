1fe38b522fe9629a88138abf887965d4915a8118
Oatchat <oatchat@Oatchats-MacBook-Air.local>
2026-01-28 23:28:49 +0700

    Add jwtSecret fallback for users-permissions plugin to fix Render deployment

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,9 @@
-export default () => ({
+export default ({ env }) => ({
+    'users-permissions': {
+        config: {
+            jwtSecret: env('JWT_SECRET', 'toset_jwt_secret_12345'),
+        },
+    },
     'content-releases': {
         enabled: false,
     },
